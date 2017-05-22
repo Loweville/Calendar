fac14a224cf6931182f45f5125f05fb3b1b0f710
Jensen Lowe <jensenlowe@yahoo.co.uk>
2017-05-22 20:53:02 +0100

    Uploaded Codebase

---
+++ Application/CalendarApplication/IAppointment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    public interface IAppointment
+    {
+        DateTime Start{ get; }
+        int Length{ get; }
+        string DisplayableDescription{ get; }
+        bool OccursOnDate(DateTime date);
+    }
+}
---
+++ Application/CalendarApplication/IAppointments.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar
+{
+    public interface IAppointments : IList<IAppointment>
+    {
+        bool Load();
+        bool Save();
+        IEnumerable<IAppointment> GetAppointmentsOnDate(DateTime date);
+    }
+}
